cc8a8acc812711777beb7b9018f878c398872c0b
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-02-18 23:21:32 +0530

    experimenting with 3js

--- src/App.js
+++ src/App.js
@@ -2,29 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import './styles/prompt.css';
-import Header from './components/Header';
+
+import { createRoot } from 'react-dom/client';
+import { Canvas } from '@react-three/fiber';
+import { Loader } from '@react-three/drei';
 function App() {
+
   return (
     <>
-    
+
       <div class="landing">
         <div class="landing-content">
           <h1>Hello there!</h1>
           <p>Get suggestions for your next movie, tv show, song, book or anime!</p>
-          <button class="landing-btn" onclick="openNav()">Get Started</button>
+          <button className="landing-btn" onclick="openNav()">Get Started</button>
         </div>
         <div>
-          <canvas class="webgl">
-            {/* <!--this is where the 3js obj loads--> */}
-          </canvas>
+          <div id='webgl'>
+            <Canvas >
+              <ambientLight intensity={0.5} />
+              <directionalLight color='red' position={[0, 10, 0]} />
+              <mesh>
+                
+                <meshStandardMaterial attach="material" color="hotpink" />
+              </mesh>
+            </Canvas>
+
+          </div>
+
         </div>
       </div>
 
       <div class="content">
-        <div class="pop" onclick="openNav()"><i class='bx bxs-hand-right'></i></div>
         {/* <!--this is where my content should go--> */}
         <div class="prompt">
-          <label for="prompt-text" class="prompt-text">
+          <label htmlFor="prompt-text" class="prompt-text">
             Enter your Prompt here :
           </label>
           <textarea name="" id="prompt-text" cols="30" rows="10"></textarea>
@@ -49,10 +61,11 @@ function App() {
           </div>
         </div>
       </div>
-      <img src="assets/load.gif" alt="" class="load"/>
-        <div class="overlay"></div>
-      </>
-      );
+      <img src="assets/load.gif" alt="" class="load" />
+      <div class="overlay"></div>
+    </>
+  );
+
 }
 
-      export default App;
+export default App;
