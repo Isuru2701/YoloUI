ec1ad3e9afe42cfd2c35072ac344b7c180df0838
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-02-19 11:22:32 +0530

    updates to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,19 +8,34 @@ import '../styles/header.css';
 function Header() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
-            <Container>
+            <Container style={{alignItems: 'center'}}>
                 <Navbar.Brand href="/"><h1><strong>YOLO</strong></h1></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/creators">Creators</Nav.Link>
-                        <Nav.Link href="/developers">Developers</Nav.Link>
+                        <Nav.Link href="/creators"><Button variant='secondary'>Creators</Button></Nav.Link>
+                        <Nav.Link href="/developers"><Button variant='secondary'>Developers</Button></Nav.Link>
+                        <Nav.Link>
+                            <Button
+                                variant='secondary'
+                                style={{
+                                    background: 'linear-gradient(to right, #ffd700, #ffcc00)',
+                                    boxShadow: '0 3px 10px 0 rgba(255,215,0,0.3)',
+                                    border: 'none',
+                                }}
+                            >
+                                <a href='/profile' style={{ color: 'black' }}>Explore Premium</a>
+                            </Button>
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
             <div>
-            <Button variant='tertiary'><a href='/register'>Register</a></Button>
-            <Button variant='tertiary'><a href='/login'>Login</a></Button>
+                <Button variant='secondary' style={{marginRight: 2}}><a href='/register'>Register</a></Button>
+
+
+
+                <Button variant='secondary'><a href='/login'>Login</a></Button>
             </div>
 
         </Navbar>
